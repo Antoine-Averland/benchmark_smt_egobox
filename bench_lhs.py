6245b08bfa69d5b1e0be19fbecf8dc54e5fdcec6
ANTOINE AVERLAND <antoine.averland@onera.fr>
2024-02-06 17:07:05 +0100

    benchmark for various dimensions and number of points

--- bench_lhs.py
+++ bench_lhs.py
@@ -17,36 +17,46 @@ matrice100 = np.full((100, 2), [0, 1])
 matrice500 = np.full((500, 2), [0, 1])
 
 list_xlimits = [matrice5, matrice20, matrice50, matrice100, matrice500]
+#list_num_points = [10, 50, 200, 800, 1500]
+list_num_points = [10, 13, 15]
 list_smt = []
 list_egobox = []
 list_time_smt = []
 list_time_egobox = []
 
-def smt_lhs(xlimits):
-    num = 10
+def smt_lhs(xlimits, num_points):
     sampling = LHS(xlimits=xlimits, criterion="ese", random_state=42)
-    doe1 = sampling(num)
+    doe1 = sampling(num_points)
     return doe1
 
-def egobox_lhs(xlimits):
+def egobox_lhs(xlimits, num_points):
     xspecs = egx.to_specs(xlimits)
-    doe2 = egx.lhs(xspecs, 10)
+    doe2 = egx.lhs(xspecs, num_points)
     return doe2
 
-# Benchmark pour SMT
+# Benchmark for SMT
 for xlimits in list_xlimits:
-    result_smt = timeit.timeit(lambda: smt_lhs(xlimits), number=5)
-    average_time_smt = result_smt / 5
-    list_time_smt.append(average_time_smt)
+    for num_points in list_num_points:
+        result_smt = timeit.timeit(lambda: smt_lhs(xlimits, num_points), number=5)
+        average_time_smt = result_smt / 5
+        list_time_smt.append(average_time_smt)
+        print(f"je suis dans le bench de smt avec {xlimits} pour {num_points} points")
 
-# Benchmark pour EGObox
+# Benchmark for EGObox
 for xlimits in list_xlimits:
-    result_egob = timeit.timeit(lambda: egobox_lhs(xlimits), number=5)
-    average_time_egob = result_egob / 5
-    list_time_egobox.append(average_time_egob)
+    for num_points in list_num_points:
+        result_egob = timeit.timeit(lambda: egobox_lhs(xlimits, num_points), number=5)
+        average_time_egob = result_egob / 5
+        list_time_egobox.append(average_time_egob)
+        print(f"je suis dans le bench de egobox avec {xlimits} pour {num_points} points")
 
 # Affichage des résultats
-for i, (average_time_smt, average_time_egob) in enumerate(zip(list_time_smt, list_time_egobox), start=1):
-    print(f"Average time for xlimits {i} for SMT: {average_time_smt} seconds")
-    print(f"Average time for xlimits {i} for EGObox: {average_time_egob} seconds")
-    print()
+for i, xlimits in enumerate(list_xlimits, start=0):
+    for j, num_points in enumerate(list_num_points):
+        idx = i * len(list_num_points) + j
+        average_time_smt = list_time_smt[idx]
+        average_time_egob = list_time_egobox[idx]
+        print(f"Average time for xlimits {i} with {num_points} points for SMT: {average_time_smt} seconds")
+        print(f"Average time for xlimits {i} with {num_points} points for EGObox: {average_time_egob} seconds")
+        print()
+

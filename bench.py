a04d91b0faa609110fbf5e8908cc7069023f3cdd
ANTOINE AVERLAND <antoine.averland@onera.fr>
2024-02-09 14:54:50 +0100

    refactoring of diagram_generator, and sort of the dimensions on the x axis

--- bench.py
+++ bench.py
@@ -1,103 +1,60 @@
-"""
-Author: Antoine Averland <antoine.averland@onera.fr>
-benchmark comparison between smt and egobox
-"""
-
 from smt.sampling_methods import LHS
 import egobox as egx
 import numpy as np
 import timeit
 import csv
 
-dimensions = [(5, 2), (20, 2), (50, 2), (100, 2), (500, 2)]
-
-list_xlimits = [np.full(dim, [0, 1]) for dim in dimensions]
-
-list_num_points = [10, 50, 100, 250, 500, 1000]
-# list_num_points = [10, 13, 15]
-
-
-list_time_smt = []
-list_time_egobox = []
+DIMENSIONS = [5, 10, 20]  # , 100]
+NB_POINTS = [10, 13, 15]
+NB_ITER = 5
+LIBRARIES = ["SMT", "EGOBOX"]
 
 
 def smt_lhs(xlimits, num_points):
     sampling = LHS(xlimits=xlimits, criterion="ese", random_state=42)
-    doe1 = sampling(num_points)
-    return doe1
+    sampling(num_points)
 
 
 def egobox_lhs(xlimits, num_points):
     xspecs = egx.to_specs(xlimits)
-    doe2 = egx.lhs(xspecs, num_points)
-    return doe2
+    egx.lhs(xspecs, num_points)
+
+
+ALGOS = {"SMT": smt_lhs, "EGOBOX": egobox_lhs}
+
+
+def run_benchmark():
+    result = []
+    for lib in LIBRARIES:
+        for dim in DIMENSIONS:
+            xlimits = np.full((dim, 2), [0, 1])
+            for num_points in NB_POINTS:
+                print(
+                    f"Running benchmark with {lib} for {xlimits.shape} and {num_points} points"
+                )
+                time = timeit.timeit(
+                    lambda: ALGOS[lib](xlimits, num_points), number=NB_ITER
+                )
+                res = {
+                    "lib": lib,
+                    "dim": dim,
+                    "nb_points": num_points,
+                    "time": time / NB_ITER,
+                }
+                print(res)
+                result.append(res)
+    return result
+
+
+def write_to_csv(csv_filename, result):
+    with open(csv_filename, mode="w", newline="") as csvfile:
+        headers = result[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+
+        for line in result:
+            writer.writerow(line)
 
 
 if __name__ == "__main__":
-    # Benchmark for SMT
-    for xlimits in list_xlimits:
-        for num_points in list_num_points:
-            print(
-                f"je suis dans le bench de smt avec {xlimits} pour {num_points} points"
-            )
-            result_smt = timeit.timeit(lambda: smt_lhs(xlimits, num_points), number=5)
-            average_time_smt = result_smt / 5
-            list_time_smt.append(average_time_smt)
-
-    # Benchmark for EGObox
-    for xlimits in list_xlimits:
-        for num_points in list_num_points:
-            print(
-                f"je suis dans le bench de egobox avec {xlimits} pour {num_points} points"
-            )
-            result_egob = timeit.timeit(
-                lambda: egobox_lhs(xlimits, num_points), number=5
-            )
-            average_time_egob = result_egob / 5
-            list_time_egobox.append(average_time_egob)
-
-    # print of the results
-    for i, xlimits in enumerate(list_xlimits, start=0):
-        for j, num_points in enumerate(list_num_points):
-            idx = i * len(list_num_points) + j
-            average_time_smt = list_time_smt[idx]
-            average_time_egob = list_time_egobox[idx]
-            print(
-                f"Average time for xlimits {i} with {num_points} points for SMT: {average_time_smt} seconds"
-            )
-            print(
-                f"Average time for xlimits {i} with {num_points} points for EGObox: {average_time_egob} seconds"
-            )
-            print()
-
-    # writing in csv file
-    csv_filename = "results_benchmark.csv"
-
-    with open(csv_filename, mode="w", newline="") as file:
-        writer = csv.writer(file)
-        headers = ["Program", "Matrix", "Num_Points", "Average_Time_(seconds)"]
-        writer.writerow(headers)
-
-        for i, xlimits in enumerate(list_xlimits, start=0):
-            for j, num_points in enumerate(list_num_points):
-                idx = i * len(list_num_points) + j
-                average_time_smt = list_time_smt[idx]
-                row = [
-                    "SMT",
-                    f"{xlimits.shape}",
-                    num_points,
-                    "{:3f}".format(average_time_smt),
-                ]
-                writer.writerow(row)
-
-        for i, xlimits in enumerate(list_xlimits, start=0):
-            for j, num_points in enumerate(list_num_points):
-                idx = i * len(list_num_points) + j
-                average_time_egob = list_time_egobox[idx]
-                row = [
-                    "EGObox",
-                    f"{xlimits.shape}",
-                    num_points,
-                    "{:.3f}".format(average_time_egob),
-                ]
-                writer.writerow(row)
+    write_to_csv("results_benchmark2.csv", run_benchmark())
--- diagram_generator.py
+++ diagram_generator.py
@@ -1,19 +1,28 @@
-"""
-Author: Antoine Averland <antoine.averland@onera.fr>
-create files with a bar chart for the comparisons.
-"""
-
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
+data = {"SMT": {}, "EGOBOX": {}}
+# NB_POINTS = [10, 50, 100, 250, 500, 1000]
+NB_POINTS = [10, 13, 15]
+CSV_FILENAME = "results_benchmark2.csv"
 
-def create_chart(matrix_dimensions, list_num_points, data):
-    for npoints in list_num_points:
+
+def sort_dimensions():
+    matrix_dimensions = list(set(data["SMT"].keys()))
+    nb_matrix = [int(dim) for dim in matrix_dimensions]
+    sort_matrix = sorted(nb_matrix)
+    sort_matrix_str = [str(number) for number in sort_matrix]
+    print(sort_matrix_str)
+    return sort_matrix_str
+
+
+def create_chart(matrix_dimensions):
+    for npoints in NB_POINTS:
         fig, ax = plt.subplots()
         bar_width = 0.35
 
-        for i, program in enumerate(["SMT", "EGObox"]):
+        for i, program in enumerate(["SMT", "EGOBOX"]):
             matrix = [data[program][matrix][npoints] for matrix in matrix_dimensions]
 
             ax.bar(
@@ -34,12 +43,8 @@ def create_chart(matrix_dimensions, list_num_points, data):
         plt.show()
 
 
-if __name__ == "__main__":
-    csv_filename = "results_benchmark2.csv"
-
-    data = {"SMT": {}, "EGObox": {}}
-
-    with open(csv_filename, mode="r") as file:
+def read_from_csv():
+    with open(CSV_FILENAME, mode="r") as file:
         reader = csv.DictReader(file)
         for row in reader:
             program = row["lib"]
@@ -53,7 +58,7 @@ if __name__ == "__main__":
             if num_points not in data[program][matrix]:
                 data[program][matrix][num_points] = average_time
 
-    matrix_dimensions = list(set(data["SMT"].keys()))
-    list_num_points = [10, 50, 100, 250, 500, 1000]
 
-    create_chart(matrix_dimensions, list_num_points, data)
+if __name__ == "__main__":
+    read_from_csv()
+    create_chart(sort_dimensions())

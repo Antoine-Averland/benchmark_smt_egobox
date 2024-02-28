9a28334b8cb23328aeed6150d66196f0c7767c60
ANTOINE AVERLAND <antoine.averland@onera.fr>
2024-02-28 13:52:51 +0100

    new test to verify if the kriging executed well

---
+++ test_kriging.py
@@ -0,0 +1,43 @@
+import unittest
+import numpy as np
+from smt.surrogate_models import KRG
+from smt.sampling_methods import LHS
+from smt.problems import Sphere
+from smt.surrogate_models import GPX
+
+
+class TestKriging(unittest.TestCase):
+    def test_kriging(self):
+        ndim = 2
+        num = 50
+        problem = Sphere(ndim=ndim)
+        xlimits = problem.xlimits
+        sampling = LHS(xlimits=xlimits, criterion="ese", random_state=42)
+
+        xt = sampling(num)
+        yt = problem(xt)
+
+        sm1 = KRG(theta0=[1e-2], nugget=1e-5)
+        sm1.set_training_values(xt, yt)
+        sm1.train()
+
+        sm2 = GPX()
+        sm2.set_training_values(xt, yt)
+        sm2.train()
+
+        xe = sampling(10)
+        ye = problem(xe)
+
+        ytest1 = sm1.predict_values(xe)
+        e_error1 = np.linalg.norm(ytest1 - ye) / np.linalg.norm(ye)
+        print(e_error1)
+
+        ytest2 = sm1.predict_values(xe)
+        e_error2 = np.linalg.norm(ytest2 - ye) / np.linalg.norm(ye)
+        print(e_error2)
+
+        self.assertAlmostEqual(e_error1, e_error2, places=5)
+
+
+if __name__ == "__main__":
+    unittest.main()
